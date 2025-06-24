a966d5b8cd99a4eba8c1981704df57cf6f126944
hyeon3233 <yeonho3233@naver.com>
2025-06-24 11:39:41 +0900

    Vue memo 작업

---
+++ src/views/Form.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive } from 'vue';
+import { HttpService } from '@/services/HttpService';
+import { useRouter } from 'vue-router';
+
+const httpService = new HttpService();
+
+const router = useRouter();
+
+const state = reactive({
+  memo: {
+    title: '',
+    content: '',
+  },
+});
+
+const subimt = () => {
+  httpService.addItem(state.memo);
+  alert('저장했습니다.');
+  router.push({ path: '/' });
+};
+</script>
+
+<template>
+  <h1>From.vue</h1>
+</template>
+
+<style scoped></style>
